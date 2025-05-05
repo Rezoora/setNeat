2294e691dfdbb9de1f23c8d4dd8490063b0b1f88
Hakmirzaev Bekhruzjon <hakmirzaevbekhruzjon@gmail.com>
2025-05-05 12:26:40 +0200

    Initial Commit

---
+++ Table Etiquette/Table_EtiquetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Table_EtiquetteApp.swift
+//  Table Etiquette
+//
+//  Created by Bekhruzjon Hakmirzaev on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Table_EtiquetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
