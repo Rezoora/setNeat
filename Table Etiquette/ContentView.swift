31114982b976f02edb344b896e0a729afe53217f
Hakmirzaev Bekhruzjon <hakmirzaevbekhruzjon@gmail.com>
2025-05-08 09:31:39 +0200

    first prototype

--- Table Etiquette/ContentView.swift
+++ Table Etiquette/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var searchStyle: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                NavigationLink(destination: BasicTabelView(), label: {
+                    ZStack(alignment: .bottomLeading) {
+                        Image("basic")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(height: 200)
+                        Rectangle().fill(LinearGradient(colors: [.clear, .accentColor.opacity(0.3)], startPoint: .trailing, endPoint: .leading))
+                        VStack(alignment: .leading) {
+                            Text("Basic")
+                                .foregroundStyle(.white)
+                                .font(.title).bold()
+                        }.padding()
+                    }.cornerRadius(30)
+                })
+            }.padding()
+            .navigationTitle("Table Etiquette")
+            .searchable(text: $searchStyle)
         }
-        .padding()
     }
 }
 
